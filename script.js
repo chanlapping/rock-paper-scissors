e603bd53ac74d9b3d241305f95d3615402aee034
chan lap ping <chanlapping64@gmail.com>
2022-10-17 13:05:10 +0800

    Add playRound function

--- script.js
+++ script.js
@@ -15,3 +15,40 @@ function getComputerChoice() {
 
     return "Scissors";
 }
+
+// function playRound
+// input: player selection, computer selection
+// return a string - declaring who is the winner
+// player selection should be case insensitive
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    if (playerSelection === computerSelection ) {
+        return "Draw";
+    }
+    if (playerSelection === "rock" && computerSelection === "paper") {
+        return "Computer wins. Rock loses to paper.";
+    }
+    if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You win. Rock beats scissors.";
+    }
+    if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "Computer wins. Paper loses to scissors.";
+    }
+    if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You win. Paper beats rock.";
+    }
+    if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "Computer wins. Scissors lose to rock.";
+    }
+    if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You win. Scissors beat paper.";
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playerSelection);
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
